9d86d4c5d178169266ec1b7585abf341ca21e52d
Madhav Sharma <Madhav2580sharma@gmail.com>
2026-04-16 21:31:15 +1000

    Implement random fruit guessing game in main function

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,42 @@
+use std::io::{self, Write};
+
 // use std::any::type_name;
 // use std::io::{self, Write};
+use rand::seq::IndexedRandom;
 
 // fn print_type_of<T>(_: &T) {
 //     println!("{}", type_name::<T>());
 // }
 
-
 fn main() {
+    let guess_list: [&str; 5] = ["mango", "apple", "banana", "grapes", "orange"];
+
+    let mut rng = rand::rng();
+
+    loop {
+        if let Some(random) = guess_list.choose(&mut rng) {
+            println!("Random Fruit: {}", random);
+            let mut input = String::new();
+            print!("Enter a fruit name: ");
+            io::stdout().flush().unwrap();
+            io::stdin().read_line(&mut input).expect("fail to readline");
+            if input.trim().to_lowercase() == *random {
+                println!("Congratulations! You guessed the correct fruit.");
+                break;
+            } else {
+                println!("Sorry! The correct fruit was: {}", random);
+            }
+        }
+    }
 
     // let x = 10;
     // print_type_of(&x);
-
 
     // let mut input = String::new();
     // print!("Enter a number: ");
     // io::stdout().flush().unwrap();
     // io::stdin().read_line(&mut input).expect("fail to readline");
     // println!("You entered: {}", input);
-
-
 }
 
 // fn process_integer(item:String){
@@ -29,52 +47,48 @@ fn main() {
 //     return item1*item2;
 // }
 
-fn _variable_and_function(){
+fn _variable_and_function() {
     // Scalar Types
 
     //     Integer
 
     //     Floating Pointer
 
-    // Compound Types 
+    // Compound Types
 
     //     Primitive Types
-        
+
     //         Array
     //         Tuple
-            // let emp_info:(&str, u8) = ("Mandy", 24);
-            // let emp_name: &str = emp_info.0;
-            // let emp_age: u8 = emp_info.1;
+    // let emp_info:(&str, u8) = ("Mandy", 24);
+    // let emp_name: &str = emp_info.0;
+    // let emp_age: u8 = emp_info.1;
 
-            // let (employee_name, employee_age) = emp_info;
+    // let (employee_name, employee_age) = emp_info;
 
-
-
-            // println!("Employee Name= {} and Employee Age= {}", employee_name, employee_age);
-            // println!("Employee Name= {} and Employee Age= {}", emp_name, emp_age);
+    // println!("Employee Name= {} and Employee Age= {}", employee_name, employee_age);
+    // println!("Employee Name= {} and Employee Age= {}", emp_name, emp_age);
 
     //     Complex Types
 
     //         Strings
-                // &str - Fixed Length Strings - Special memory
-                // String - Dyanamic Length Strings - Heap memory
+    // &str - Fixed Length Strings - Special memory
+    // String - Dyanamic Length Strings - Heap memory
 
-                // let mut string_literal:&str = "Hello";
-                // println!("This is String literal: {}", string_literal);
+    // let mut string_literal:&str = "Hello";
+    // println!("This is String literal: {}", string_literal);
 
-                // let mut string_literal:String = String::from("Hello");
-                // string_literal.push_str("string");
-                // println!("This is String literal: {}", string_literal);
+    // let mut string_literal:String = String::from("Hello");
+    // string_literal.push_str("string");
+    // println!("This is String literal: {}", string_literal);
 
-
-
-                // let num1: u8 = 10;
-                // let num2: u8 = 12;
-                // let result: u8 = add(num1, num2);
-                // println!("The sum of num1 and num2 is {}", result);
+    // let num1: u8 = 10;
+    // let num2: u8 = 12;
+    // let result: u8 = add(num1, num2);
+    // println!("The sum of num1 and num2 is {}", result);
 }
 
-fn _ownership_and_borrowing(){
+fn _ownership_and_borrowing() {
     // Ownership
     // One value = one owner
     // Only one owner at a time
@@ -89,7 +103,7 @@ fn _ownership_and_borrowing(){
     // let w1 = &mut word;
     // w1.push_str(" string");
     // println!("w1 is {}", w1);
-    
+
     // let w2 = &mut word;
     // w2.push_str(" string");
     // println!("w1 is {}", w2);
@@ -97,6 +111,4 @@ fn _ownership_and_borrowing(){
     // let r1 = &word;
     // let r2 = &word;
     // println!("r1 is {} and r2 is {}", r1, r2);
-
-
 }
